a2ed8fa2b635f5a867785b03b8dd6688c1ce9d64
Madhuvanthi Hemanathan <madhunarmi@gmail.com>
2021-02-19 21:12:00 +0530

    Refactored the folder structure

--- src/main/java/model/Map.java
+++ src/main/java/model/Map.java
@@ -1,13 +1,73 @@
 package model;
 
-/**
- * Concrete Map class for creating instances
- *   @author Neona Pinto
- *   @author Dhananjay Narayan
- *   @author Surya Manian
- *   @author Madhuvanthi Hemanathan
- *   @author Prathika Suvarna
- */
-public class Map extends AbstractMap {
+import java.util.List;
 
+/**
+ * Concrete Class to set and get all the properties of the Map.
+ *
+ * @author Neona Pinto
+ * @author Dhananjay Narayan
+ * @author Surya Manian
+ * @author Madhuvanthi Hemanathan
+ * @author Prathika Suvarna
+ * @version 1.0.0
+ */
+public class Map {
+    private List<Continent> d_continents;
+    private List<Country> d_countries;
+    private List<Player> d_players;
+
+    /**
+     * Get the list of all the continents
+     *
+     * @return d_continents List of the continents
+     */
+    public List<Continent> getContinents() {
+        return d_continents;
+    }
+
+    /**
+     * Set the list of Continents
+     *
+     * @param p_continents List of continents
+     */
+    public void setContinents(List<Continent> p_continents) {
+        this.d_continents = p_continents;
+    }
+
+    /**
+     * Get the list of countries
+     *
+     * @return d_countries List of the countries
+     */
+    public List<Country> getCountries() {
+        return d_countries;
+    }
+
+    /**
+     * Set the list of countries
+     *
+     * @param p_countries List of countries
+     */
+    public void setCountries(List<Country> p_countries) {
+        this.d_countries = p_countries;
+    }
+
+    /**
+     * Get the list of players
+     *
+     * @return d_players List of players
+     */
+    public List<Player> getPlayers() {
+        return d_players;
+    }
+
+    /**
+     * Set the list of players
+     *
+     * @param p_players List of players
+     */
+    public void setPlayers(List<Player> p_players) {
+        this.d_players = p_players;
+    }
 }
--- src/main/java/model/Order.java
+++ src/main/java/model/Order.java
@@ -1,13 +1,109 @@
 package model;
 
 /**
- *  A class to set the instances of order
- *   @author Neona Pinto
- *   @author Dhananjay Narayan
- *   @author Surya Manian
- *   @author Madhuvanthi Hemanathan
- *   @author Prathika Suvarna
+ * Concrete Class to manage the orders of the players
+ *
+ * @author Neona Pinto
+ * @author Dhananjay Narayan
+ * @author Surya Manian
+ * @author Madhuvanthi Hemanathan
+ * @author Prathika Suvarna
+ * @version 1.0.0
  */
-public class Order extends AbstractOrder {
+public class Order {
+    private int d_id;
+    private OrderType d_type;
+    private Country d_from;
+    private Country d_to;
+    private int d_reinforcements;
 
+    /**
+     * A function to get the ID of the player
+     *
+     * @return the ID of the player
+     */
+    public int getId() {
+        return d_id;
+    }
+
+    /**
+     * A function to set the ID of the player
+     *
+     * @param p_id ID of the player to be set
+     */
+    public void setId(int p_id) {
+        this.d_id = p_id;
+    }
+
+    /**
+     * A function to fetch the type of the order
+     *
+     * @return The Order Type
+     */
+    public OrderType getType() {
+        return d_type;
+    }
+
+    /**
+     * A function to set the order object d_type with p_type
+     *
+     * @param p_type Object of the class OrderType
+     */
+    public void setD_type(OrderType p_type) {
+        this.d_type = p_type;
+    }
+
+    /**
+     * A function to get where the army is coming from
+     *
+     * @return the country where the army is coming from
+     */
+    public Country getFrom() {
+        return d_from;
+    }
+
+    /**
+     * A function to set the the country where the army is coming from
+     *
+     * @param p_from Object of Class Country
+     */
+    public void setFrom(Country p_from) {
+        this.d_from = p_from;
+    }
+
+    /**
+     * A function to get the country where the army is being sent to
+     *
+     * @return the country where army is being sent
+     */
+    public Country getTo() {
+        return d_to;
+    }
+
+    /**
+     * A function to set the country where the army is being sent to
+     *
+     * @param p_to Object of Class Country
+     */
+    public void setTo(Country p_to) {
+        this.d_to = p_to;
+    }
+
+    /**
+     * A function to get the number of reinforcements
+     *
+     * @return number of reinforcements
+     */
+    public int getReinforcements() {
+        return d_reinforcements;
+    }
+
+    /**
+     * A function to set the number of reinforcements
+     *
+     * @param p_reinforcements the number of reinforcements
+     */
+    public void setReinforcements(int p_reinforcements) {
+        this.d_reinforcements = p_reinforcements;
+    }
 }
