ed5275349a7e78a0afafb89b2c269a617d270e3d
Dhananjay Narayan <dhananjayashwath@gmail.com>
2021-02-24 03:14:59 +0530

    Added view part for Country details

--- src/main/java/view/MapView.java
+++ src/main/java/view/MapView.java
@@ -51,6 +51,68 @@ public class MapView {
         }
         System.out.format("+------------------+%n");
 
+
+        // Showing Countries in the Continent and their details
+        System.out.println("\nThe countries in this Map and their details are : \n");
+
+        Iterator<Entry<String, Continent>> iteratorForContinent = gameMap.getContinents().entrySet()
+                .iterator();
+
+        //	table = "|%-14s|%-23s|%-18s|%-28s|%-15s|%-15s|%n";
+        table = "|%-23s|%-18s|%-60s|%-15s|%n";
+
+        System.out.format(
+                "+--------------+-----------------------+------------------+----------------------------+---------------+---------------+%n");
+        System.out.format(
+                "     Country's name     | Continent's Name |   Neighbour Countries                                      | No. of armies |%n");
+        System.out.format(
+                "+--------------+-----------------------+------------------+----------------------------+---------------+---------------+%n");
+
+        while (iteratorForContinent.hasNext()) {
+            Map.Entry<String, Continent> continentMap = (Map.Entry<String, Continent>) iteratorForContinent.next();
+            String continentId = (String) continentMap.getKey();
+            Continent continent = gameMap.getContinents().get(continentId); // to get the continent by its ID(Name)
+            //ListIterator<Country> listIterator = continent.getCountries().listIterator();
+            Iterator<Country> listIterator = continent.getCountries().iterator();
+
+            while (listIterator.hasNext()) {
+
+                Country country = (Country) listIterator.next();
+
+
+                //System.out.format(table, country.getId(), country.getName(), continent.getName(),country.getNeighbors(),country.getArmies(), country.getPlayer().getName());
+                System.out.format(table, country.getName(), continent.getName(),country.getNeighborsName(),country.getArmies());
+
+            }
+        }
+
+        System.out.format(
+                "+--------------+-----------------------+------------------+----------------------------+---------------+---------------+%n");
+
+
+
+
+        //Showing the Ownership of the players
+        System.out.println("\nThe Map ownership of the players are : \n");
+        String table1 = "|%-15s|%-30s|%-21d|%n";
+
+        System.out.format(
+                "+---------------+-----------------------+------------------------------+---------------------+%n");
+        System.out.format(
+                "| Player's name |    Continent's Controlled    | No. of Armies Owned |%n");
+        System.out.format(
+                "+---------------+-----------------------+------------------------------+---------------------+%n");
+
+        //Will test this code after the players are assigned. Before this everything is perfect and tested, and need not be changed.
+
+/*
+        	players.forEach((key, value) ->
+        System.out.format(table1, players.getName(), players.getCapturedCountries(),players.getNumberOfArmies()); //Have to add a function GetNumberOfArmies in Player
+
+
+
+        	System.out.format("+---------------+-----------------------+------------------------------+---------------------+%n");
+*/
     }
 
 
