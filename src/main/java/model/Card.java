6e69a8303d87b83ae98ac9cddb17ba26cd55bbfc
PrathikaSuvarna <prathikasuvarna96gmail.com>
2021-03-20 21:47:36 +0530

    Added JavaDoc for Card

--- src/main/java/model/Card.java
+++ src/main/java/model/Card.java
@@ -14,11 +14,23 @@ public enum Card{
 
     private final String d_CardType;
 
+    /**
+     * Instantiates a new card.
+     *
+     * @param p_CardType the card type
+     */
 
     Card(String p_CardType){
         this.d_CardType = p_CardType;
     }
 
+    /**
+     * Every time player conquer one country, he can get a random card by this method
+     * getEnumConstants function can get the list of cards.
+     *
+     * @param p_CardObject the card object
+     * @return one random kind of card
+     */
     public static Card getCard(Class<Card> p_CardObject)
     {
         return p_CardObject.getEnumConstants()[new Random().nextInt(p_CardObject.getEnumConstants().length)];
