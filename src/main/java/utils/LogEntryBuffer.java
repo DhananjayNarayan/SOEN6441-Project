5b414a50512bf49abe5c1e945a82c7999c6b548e
maniansurya99 <maniansurya99@gmail.com>
2021-03-22 03:16:19 +0530

    Added javadoc for LogEntryBuffer

--- src/main/java/utils/LogEntryBuffer.java
+++ src/main/java/utils/LogEntryBuffer.java
@@ -4,18 +4,35 @@ import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.PrintWriter;
 
+/**
+ * This class gets all the actions of the game. It is an Observable.
+ * @author Dhananjay Narayan
+ * @author Surya Manian
+ *
+ */
 public class LogEntryBuffer {
 
     private LogEntryWriter d_lew = new LogEntryWriter(); //d_lew is an observer object
 
+    /**
+     * This method gets the information from the game and notifies the Observer.
+     * @param p_s The message to be notified
+     */
     public void logInfo(String p_s) {
         notifyObservers(p_s);
     }
 
+    /**
+     * This method updates the Observer with the message.
+     * @param p_s The message to be updated
+     */
     public void notifyObservers(String p_s) {
         d_lew.update(p_s);
     }
 
+    /**
+     * This method is used to clear the log file before a new game starts.
+     */
     public void clearNewFile() {
         PrintWriter l_writeData = null;
         String l_filename ="demolog";
