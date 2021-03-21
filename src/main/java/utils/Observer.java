0b719f2203ef4e5c63854e2d48bde9667adc03ad
Dhananjay Narayan <dhananjayashwath@gmail.com>
2021-03-22 03:24:51 +0530

    Added javadoc for Observer

--- src/main/java/utils/Observer.java
+++ src/main/java/utils/Observer.java
@@ -1,5 +1,14 @@
 package utils;
 
+/**
+ * An interface for implementation of Observer with an update function
+ * @author Dhananjay Narayan
+ */
 public interface Observer {
+
+    /**
+     * Function to update the message for the observer
+     * @param p_s the message to be updated
+     */
     void update(String p_s);
 }
