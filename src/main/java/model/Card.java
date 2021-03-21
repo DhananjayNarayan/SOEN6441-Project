149172b9c8e4c85c3932a5c75e3df0d1ba1bbde3
PrathikaSuvarna <prathikasuvarna96gmail.com>
2021-03-21 15:38:56 +0530

    Card creation modified

--- src/main/java/model/Card.java
+++ src/main/java/model/Card.java
@@ -6,34 +6,42 @@ import java.util.Random;
  * This is an Enum for Cards.
  *
  */
-public enum Card{
-    BOMB("Bomb"),
-    BLOCKADE("Blockade"),
-    AIRLIFT("Airlift"),
-    DIPLOMACY("Diplomacy");
+public class Card {
 
     private final String d_CardType;
 
-    /**
-     * Instantiates a new card.
-     *
-     * @param p_CardType the card type
-     */
+    public enum CardType {
+        BOMB,
+        BLOCKADE,
+        AIRLIFT,
+        DIPLOMACY;
 
-    Card(String p_CardType){
-        this.d_CardType = p_CardType;
+        public static String getRandomCard()
+        {
+            Random random = new Random();
+            CardType c= values()[random.nextInt(values().length)];
+            return c.name();}
     }
 
-    /**
-     * Every time player conquer one country, he can get a random card by this method
-     * getEnumConstants function can get the list of cards.
-     *
-     * @param p_CardObject the card object
-     * @return one random kind of card
-     */
-    public static Card getCard(Class<Card> p_CardObject)
-    {
-        return p_CardObject.getEnumConstants()[new Random().nextInt(p_CardObject.getEnumConstants().length)];
-    }
+
+        /**
+         *
+         *
+         */
+
+        public Card() {
+            d_CardType=CardType.getRandomCard();
+        }
+
+        /**
+         *
+         *
+         * @return the card type
+         */
+        public String getCardType() {
+
+            return d_CardType;
+
+        }
 }
 
