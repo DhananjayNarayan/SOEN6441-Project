1b4bf094fc8e3f027257a25e071d58f50b49cbf7
PrathikaSuvarna <prathikasuvarna96gmail.com>
2021-03-21 15:48:29 +0530

    Added Javadoc for Card Class

--- src/main/java/model/Card.java
+++ src/main/java/model/Card.java
@@ -3,18 +3,31 @@ package model;
 import java.util.Random;
 
 /**
- * This is an Enum for Cards.
+ * This is the class to create card and assign the Card Type randomly
+ *
+ * @author Prathika Suvarna
+ * @version 1.0.0
  *
  */
 public class Card {
 
     private final String d_CardType;
 
+    /**
+     * This is an Enum for Card Type
+     *
+     */
     public enum CardType {
         BOMB,
         BLOCKADE,
         AIRLIFT,
         DIPLOMACY;
+
+        /**
+         * This method assigns a Random Card from the Enum of Card types
+         *
+         * @return The random Card Name
+         */
 
         public static String getRandomCard()
         {
@@ -23,25 +36,25 @@ public class Card {
             return d_Type.name();}
     }
 
+    /**
+     * This is a Constructor for Class Card
+     *
+     */
 
-        /**
-         *
-         *
-         */
-
-        public Card() {
+    public Card()
+    {
             d_CardType=CardType.getRandomCard();
-        }
+    }
 
-        /**
-         *
-         *
-         * @return the card type
-         */
-        public String getCardType() {
+    /**
+     * This method is used to get the Card Type
+     *
+     * @return the Card Type
+     */
+    public String getCardType() {
 
             return d_CardType;
 
-        }
+    }
 }
 
