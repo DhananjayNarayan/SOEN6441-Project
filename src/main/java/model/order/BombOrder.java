6921e809821dc32a0de026d47d3eb89c6cb97460
PrathikaSuvarna <prathikasuvarna96gmail.com>
2021-03-24 19:23:35 +0530

    Bomb Card methods added

--- src/main/java/model/order/BombOrder.java
+++ src/main/java/model/order/BombOrder.java
@@ -1,9 +1,36 @@
 package model.order;
 
+import model.GameMap;
+import utils.LogEntryBuffer;
+
 /**
- * This class implements the bomb order
- *
- * @author Prathika
+ * This class implements the bomb order card
+ * 
+ * @author Prathika 
  */
-public class BombOrder {
+public class BombOrder extends Order{
+
+    private GameMap d_GameMap;
+    LogEntryBuffer d_leb = new LogEntryBuffer();
+
+    public BombOrder() {
+        super();
+        setType("bomb");
+        d_GameMap = GameMap.getInstance();
+    }
+
+    @Override
+    public boolean execute() {
+        return false;
+    }
+
+    @Override
+    public boolean validateCommand() {
+        return false;
+    }
+
+    @Override
+    public void printOrderCommand() {
+
+    }
 }
