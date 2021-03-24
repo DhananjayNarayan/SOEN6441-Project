72e76df50b0558e75e37fdfb58e92a1c686e32a4
PrathikaSuvarna <prathikasuvarna96gmail.com>
2021-03-24 21:08:07 +0530

    Validations added in bomb order

--- src/main/java/model/order/BombOrder.java
+++ src/main/java/model/order/BombOrder.java
@@ -1,6 +1,9 @@
 package model.order;
 
+import model.CardType;
+import model.Country;
 import model.GameMap;
+import model.Player;
 import utils.LogEntryBuffer;
 
 /**
@@ -11,6 +14,7 @@ import utils.LogEntryBuffer;
 public class BombOrder extends Order{
 
     private GameMap d_GameMap;
+
     LogEntryBuffer d_leb = new LogEntryBuffer();
 
     public BombOrder() {
@@ -21,16 +25,66 @@ public class BombOrder extends Order{
 
     @Override
     public boolean execute() {
+        Player l_Player = getOrderInfo().getPlayer();
+        Country l_Country = getOrderInfo().getTargetCountry();
+        if (validateCommand()) {
+            int l_Armies = l_Country.getArmies();
+            int l_NewArmies = l_Armies/2;
+            if (l_NewArmies<0){
+                l_NewArmies = 0;
+            }
+            l_Country.setArmies(l_NewArmies);
+            l_Player.removeCard(CardType.BOMB);
+            return true;
+        }
         return false;
     }
 
     @Override
     public boolean validateCommand() {
-        return false;
+        Player l_Player = getOrderInfo().getPlayer();
+        Country l_Country = getOrderInfo().getTargetCountry();
+
+        if (l_Player == null) {
+            System.out.println("The Player is not valid.");
+            return false;
+        }
+        if (!l_Player.checkIfCardAvailable(CardType.BOMB)) {
+            System.out.println("Player doesn't have Blockade Card.");
+            return false;
+        }
+        //check whether the target country belongs to the player
+        if(l_Player.getCapturedCountries().contains(l_Country)){
+            System.out.println("The player cannot destroy armies in his own country.");
+            return false;
+        }
+
+        //check if DIPLOMACY
+
+
+
+        // validate that the country is adjacent to one of the current player’s neighbors.
+        Boolean l_Adjacent = false;
+        for (Country l_PCountry : l_Player.getCapturedCountries()) {
+            for (Country l_NCountry : l_PCountry.getNeighbors()) {
+                if (l_NCountry.getName().equals(l_Country.getName())) {
+                    l_Adjacent = true;
+                    break;
+                }
+            }
+        }
+        if (!l_Adjacent){
+            System.out.println("The target country is not adjacent to one of the countries that belong to the player.");
+            return false;
+        }
+        return true;
     }
 
     @Override
     public void printOrderCommand() {
+        System.out.println("Bomb Order issued by player:" + getOrderInfo().getPlayer().getName() + " on Country" + getOrderInfo().getTargetCountry().getName());
+        System.out.println("---------------------------------------------------------------------------------------------");
 
     }
 }
+
