6c2bda6799537056872d8cf6b93cdd93deb2eed0
Madhuvanthi Hemanathan <madhunarmi@gmail.com>
2021-04-10 21:09:00 +0530

    javadoc added for new files

--- src/main/java/model/strategy/player/PlayerStrategy.java
+++ src/main/java/model/strategy/player/PlayerStrategy.java
@@ -1,8 +1,21 @@
 package model.strategy.player;
 
+/**
+ * Interface class holding the different types of player.
+ *
+ * @author Madhuvanthi Hemanathan
+ * @version 1.0.0
+ */
 public interface PlayerStrategy {
     String createCommand();
 
+    /**
+     * Method which returns the class holding the player gameplay
+     * logic based on strategy chosen.
+     *
+     * @param p_Strategy Player strategy provided
+     * @return the respective strategy class
+     */
     static PlayerStrategy getStrategy(String p_Strategy) {
         switch (p_Strategy) {
             case "human": {
