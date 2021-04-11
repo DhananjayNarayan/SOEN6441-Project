47954e6652990c80c029174cbc6731fd9eada791
PrathikaSuvarna <prathikasuvarna96gmail.com>
2021-04-11 20:58:38 +0530

    Fixed naming convention

--- src/main/java/model/strategy/player/PlayerStrategyType.java
+++ src/main/java/model/strategy/player/PlayerStrategyType.java
@@ -1,7 +1,5 @@
 package model.strategy.player;
 
-import java.util.Random;
-
     /**
      * This is the enum class for Player Strategy type
      *
@@ -28,16 +26,6 @@ import java.util.Random;
          */
         CHEATER;
 
-        /**
-         * This method assigns a Random Card from the Enum of Card types
-         *
-         * @return The random Card Name
-         */
-       /*/ public static model.CardType getRandomCard() {
-            Random d_Random = new Random();
-            model.CardType d_Type = values()[d_Random.nextInt(values().length)];
-            return d_Type;
-        }*/
     }
 
 
