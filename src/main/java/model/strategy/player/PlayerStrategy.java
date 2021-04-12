1e12c068acbb70b14249b73f5e35071a4ccd7261
PrathikaSuvarna <prathikasuvarna96gmail.com>
2021-04-13 02:47:19 +0530

    Changes in player strategy

--- src/main/java/model/strategy/player/PlayerStrategy.java
+++ src/main/java/model/strategy/player/PlayerStrategy.java
@@ -1,14 +1,22 @@
 package model.strategy.player;
 
+import controller.GamePlay;
+import model.Player;
+
 /**
  * Interface class holding the different types of player.
  *
  * @author Madhuvanthi Hemanathan
  * @version 1.0.0
  */
-public interface PlayerStrategy {
-    String createCommand();
+public abstract class PlayerStrategy {
+    abstract public String createCommand();
+    Player d_player = null;
 
+
+    PlayerStrategy(Player p_player){
+        d_player = p_player;
+    }
     /**
      * Method which returns the class holding the player gameplay
      * logic based on strategy chosen.
@@ -16,10 +24,13 @@ public interface PlayerStrategy {
      * @param p_Strategy Player strategy provided
      * @return the respective strategy class
      */
-    static PlayerStrategy getStrategy(String p_Strategy) {
+    static PlayerStrategy getStrategy(String p_Strategy,Player d_player) {
         switch (p_Strategy) {
             case "human": {
                 return new HumanStrategy();
+            }
+            case "benevolent": {
+                return new BenevolentStrategy(d_player);
             }
         }
         throw new IllegalStateException("not a valid player type");
