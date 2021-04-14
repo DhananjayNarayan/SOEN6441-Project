b2536ca0f31c014d1a4557eb13602c78d8c90c7f
maniansurya99 <maniansurya99@gmail.com>
2021-04-14 14:55:35 +0530

    Added constructor for CheaterStrategy

--- src/main/java/model/strategy/player/CheaterStrategy.java
+++ src/main/java/model/strategy/player/CheaterStrategy.java
@@ -1,12 +1,21 @@
 package model.strategy.player;
 
 import model.*;
-import model.order.*;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
 public class CheaterStrategy extends PlayerStrategy implements  Serializable {
+
+    private static final GameMap d_GameMap = GameMap.getInstance();
+
+    List<Country> l_cheatCountryList;
+
+    CheaterStrategy(Player p_player){
+        super(p_player);
+        l_cheatCountryList = new ArrayList<>();
+
+    }
 
     public String createCommand() {
 
