4e265a90a0b1bcb380b727de7ea52730fa66a5a7
PrathikaSuvarna <prathikasuvarna96gmail.com>
2021-04-14 18:01:47 +0530

    Case added in Player strategy

--- src/main/java/model/strategy/player/PlayerStrategy.java
+++ src/main/java/model/strategy/player/PlayerStrategy.java
@@ -1,21 +1,19 @@
 package model.strategy.player;
-
 import model.Player;
 
 /**
- * Interface class holding the different types of player.
+ * Abstract class holding the different types of player.
  *
  * @author Madhuvanthi Hemanathan
  * @version 1.0.0
  */
 public abstract class PlayerStrategy {
     static Player d_Player;
-    PlayerStrategy(){
 
-    }
     PlayerStrategy(Player p_Player){
         d_Player = p_Player;
     }
+
     public abstract String createCommand();
 
     /**
