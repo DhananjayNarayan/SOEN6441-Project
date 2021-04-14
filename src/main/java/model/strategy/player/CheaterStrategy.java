773b2a140bf12804427738120cff3ebb86fa5c7f
Dhananjay Narayan <dhananjayashwath@gmail.com>
2021-04-14 14:06:56 +0530

    New file for cheater strategy

---
+++ src/main/java/model/strategy/player/CheaterStrategy.java
@@ -0,0 +1,20 @@
+package model.strategy.player;
+
+import model.*;
+import model.order.*;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CheaterStrategy extends PlayerStrategy implements  Serializable {
+
+    public String createCommand() {
+
+
+
+
+
+
+        return null;
+    }
+}
