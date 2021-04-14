6a22122f805da3cf6db248db476fcb18003bc42f
Dhananjay Narayan <dhananjayashwath@gmail.com>
2021-04-14 14:48:41 +0530

    Coding conventions AggressiveStrategy

--- src/main/java/model/strategy/player/AggressiveStrategy.java
+++ src/main/java/model/strategy/player/AggressiveStrategy.java
@@ -37,13 +37,13 @@ public class AggressiveStrategy extends PlayerStrategy implements  Serializable 
      */
     public Country determineStrongestCountry(Player p_player) {
         List<Country> countryList = p_player.getCapturedCountries();
-        Country l_strongestCountry = countryList.get(0);
-        for (Country country : countryList) {
-            if (country.getArmies() > l_strongestCountry.getArmies())
-                l_strongestCountry = country;
+        Country l_StrongestCountry = countryList.get(0);
+        for (Country l_Country : countryList) {
+            if (l_Country.getArmies() > l_StrongestCountry.getArmies())
+                l_StrongestCountry = l_Country;
         }
 
-        return l_strongestCountry;
+        return l_StrongestCountry;
     }
 
     /**
@@ -69,13 +69,13 @@ public class AggressiveStrategy extends PlayerStrategy implements  Serializable 
 
         //Bomb an opponent Country if it has more than 1 army
         if(d_Player.getPlayerCards().size() > 0){
-            for(Card l_card: d_Player.getPlayerCards()){
-                if(l_card.getCardType() == CardType.BOMB) {
-                    for (Country l_c : d_Player.getCapturedCountries()) {
-                        for (Country l_b : l_c.getNeighbors()) {
-                            if ( !l_b.getPlayer().equals(d_Player) && l_b.getArmies() > 0 ) {
+            for(Card l_Card: d_Player.getPlayerCards()){
+                if(l_Card.getCardType() == CardType.BOMB) {
+                    for (Country l_C : d_Player.getCapturedCountries()) {
+                        for (Country l_B : l_C.getNeighbors()) {
+                            if ( !l_B.getPlayer().equals(d_Player) && l_B.getArmies() > 0 ) {
                                 l_Commands.add(0, "bomb");
-                                l_Commands.add(1,l_b.getName());
+                                l_Commands.add(1,l_B.getName());
                                 l_CommandsArr = l_Commands.toArray(new String[l_Commands.size()]);
                                 l_Order = new BombOrder();
                                 l_Order.setOrderInfo(OrderCreater.GenerateBombOrderInfo(l_CommandsArr, d_Player));
@@ -88,29 +88,29 @@ public class AggressiveStrategy extends PlayerStrategy implements  Serializable 
         }
 
         // Attack/Advance command
-        for (Country l_c : l_StrongCountry.getNeighbors()) {
-            if (!(l_c.getPlayer().equals(d_Player))) {
+        for (Country l_C : l_StrongCountry.getNeighbors()) {
+            if (!(l_C.getPlayer().equals(d_Player))) {
                 l_Commands.add(0, "advance");
                 l_Commands.add(1,l_StrongCountry.getName());
-                l_Commands.add(2,l_c.getName());
+                l_Commands.add(2,l_C.getName());
                 l_Commands.add(3,String.valueOf(l_StrongCountry.getArmies())) ;
                 l_CommandsArr = l_Commands.toArray(new String[l_Commands.size()]);
                 l_Order = new AdvanceOrder();
                 l_Order.setOrderInfo(OrderCreater.GenerateAdvanceOrderInfo(l_CommandsArr,d_Player));
-                l_StrongCountry = l_c;
+                l_StrongCountry = l_C;
             }
         }
 
 // Moving the armies to other next countries of players. Alternate is to move the armies to a random self owned country if we have to change.
-        for (Country l_c : l_StrongCountry.getNeighbors()) {
+        for (Country l_C : l_StrongCountry.getNeighbors()) {
             l_Commands.add(0, "advance");
             l_Commands.add(1,l_StrongCountry.getName());
-            l_Commands.add(2,l_c.getName());
+            l_Commands.add(2,l_C.getName());
             l_Commands.add(3,String.valueOf(l_StrongCountry.getArmies())) ;
             l_CommandsArr = l_Commands.toArray(new String[l_Commands.size()]);
             l_Order = new AdvanceOrder();
             l_Order.setOrderInfo(OrderCreater.GenerateAdvanceOrderInfo(l_CommandsArr,d_Player));
-            l_StrongCountry = l_c;
+            l_StrongCountry = l_C;
 
         }
 
